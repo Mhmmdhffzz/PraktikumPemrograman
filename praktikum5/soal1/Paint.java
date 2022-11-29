200298f7a164bfde8e17bc75f10f41a690219ee2
Mhmmdhffzz <104207816+Mhmmdhffzz@users.noreply.github.com>
2022-11-30 01:42:24 +0800

    Update Paint.java

--- praktikum5/soal1/Paint.java
+++ praktikum5/soal1/Paint.java
@@ -18,13 +18,5 @@ public double amount(Shape s)
 System.out.println ("Computing amount for " + s);
 return s.area()/this.coverage;
 }
-}//-----------------------------------
-// Returns the sphere as a String.
-//-----------------------------------
-
-public String toString()
-{
-return super.toString + " of radius " + radius;
-}
 }
 
